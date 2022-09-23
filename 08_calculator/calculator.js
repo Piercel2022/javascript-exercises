7243d5cf5087355146923547d5c9186b66e63559
Pierre <piercel.coder2022@gmail.com>
2022-09-23 12:03:53 +0200

    Add solution to the workflow

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
 const add = function() {
-	
+  let a = 0, b = 0;
+	return a + b;
 };
 
 const subtract = function() {
-	
+	let a = 0, b = 0;
+  return a - b;
 };
 
 const sum = function() {
+  let a = 0, b = 0;
+  return a + b
 	
 };
 
 const multiply = function() {
+  let a = 0, b = 0;
+  return a * b;
 
 };
 
 const power = function() {
+  let a = 0, b = 0;
+  let x = Math.pow(a, b);
+  return x;
 	
 };
 
 const factorial = function() {
-	
+  let num = 0;
+	 if (num == 0) 
+      return 1;
+  else {
+      return (num * factorial(num - 1));
+  }
 };
 
 // Do not edit below this line
