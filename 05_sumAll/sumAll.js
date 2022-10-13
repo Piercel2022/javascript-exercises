a21a6970c1ed00dde8e10a64b778f78d846a31ce
Pierre <piercel.coder2022@gmail.com>
2022-10-13 17:58:16 +0200

    Add the sum of all numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
+function sumAll(l, h)  {
+  let sum = 0;
+  for(let i = l; i <= h; i++){
+     sum += i;
+  };
+  return sum;
 
-};
+}
 
 // Do not edit below this line
 module.exports = sumAll;
