b4f9350a7a5e1e36926b048bb449a5eb41e8115a
Pierre <piercel.coder2022@gmail.com>
2022-10-13 16:46:25 +0200

    Add the solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
 const repeatString = function() {
-
+    var times = 0;
+    if (times < 0) return 'ERROR';
+  let string = '';
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
 const reverseString = function() {
-
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
