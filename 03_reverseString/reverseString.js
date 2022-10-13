1f2909b82fc45f2840b6f14b1d9cf25a318f93ca
Pierre <piercel.coder2022@gmail.com>
2022-10-13 16:55:08 +0200

    Add the reverse string solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-    return str.split("").reverse().join("");
+const reverseString = function(str) {
+    return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
 };
 
 // Do not edit below this line
