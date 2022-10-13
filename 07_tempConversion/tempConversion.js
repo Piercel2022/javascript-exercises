e0364cddaa5bfbd4080fe79433f094b4b63e5a7a
Pierre <piercel.coder2022@gmail.com>
2022-10-13 18:38:05 +0200

    Add the conversion to celsius to fahrenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+function ftoc(fahrenheit) {
+  celsius = (fahrenheit - 32) / 1.8;
+  return celsius;
 };
 
-const ctof = function() {
-
+function ctof(celsius) {
+  const fahrenheit = (celsius * 1.8) + 32
+  // return the variable fahrenheit as the answer
+  return fahrenheit;
 };
 
 // Do not edit below this line
