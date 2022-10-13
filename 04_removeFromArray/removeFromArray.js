25f0e12fd912f439a629cbe8f65795a5b820ac03
Pierre <piercel.coder2022@gmail.com>
2022-10-13 17:12:19 +0200

    Add the remove from array solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, value) {
+    return arr.filter(function(geeks){
+        return geeks != value;
+    });
 };
 
 // Do not edit below this line
