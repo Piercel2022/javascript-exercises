5d6cc5647ff4a3cab21f90f58aa0bf9a148d0b14
Pierre <piercel.coder2022@gmail.com>
2022-10-16 23:45:03 +0200

    Add the solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+function palindromes (str) {
+    let re = /[\W_]/g;
 
+    let lowRegStr = str.toLowerCase().replace(re, '');
+  
+    let reverseStr = lowRegStr.split('').reverse().join(''); 
+  
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
